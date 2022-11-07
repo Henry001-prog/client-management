43861176932e698514b786557ac4b71b8fa0abe8
Henry001-prog <alissonhenrique001@hotmail.com.br>
2022-11-07 20:51:48 -0300

    clients management project

--- App.tsx
+++ App.tsx
@@ -1,9 +1,4 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
  *
  * @format
  */
@@ -14,36 +9,6 @@ import {StatusBar, useColorScheme} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {RecoilRoot} from 'recoil';
 import AppRoutes from './src/Routes';
-
-// const Section: React.FC<
-//   PropsWithChildren<{
-//     title: string;
-//   }>
-// > = ({children, title}) => {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
